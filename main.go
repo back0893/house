511e8bd4bb1b5e7c609b38af24411a9939181de1
liu <liu>
2020-07-09 00:32:52 +0800

    main

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	v1 "main/routes/v1"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/world", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "ok"})
-	})
-	r.Run()
+	version1 := r.Group("v1")
+	{
+		version1.POST("/contract/add", v1.ContractAdd)
+		version1.POST("/contract/edit", v1.ContractEdit)
+		version1.POST("/contract/delete", v1.ContractDelete)
+		version1.POST("/contract/index", v1.ContractIndex)
+		version1.GET("/contract/index", v1.ContractIndex)
+
+		version1.POST("/house/add", v1.HouseAdd)
+		version1.POST("/house/edit", v1.HouseEdit)
+		version1.POST("/house/delete", v1.HouseDelete)
+		version1.POST("/house/index", v1.HousenIndex)
+		version1.GET("/house/index", v1.HousenIndex)
+
+		version1.POST("/log/add", v1.ContractLogAdd)
+		version1.POST("/log/delete", v1.ContractLogDelete)
+		version1.POST("/log/index", v1.ContractLogIndex)
+		version1.GET("/log/index", v1.ContractLogIndex)
+	}
+	r.Run(":8080")
 }
